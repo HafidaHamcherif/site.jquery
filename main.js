097d49ab68dc6ffa2cdf67cf83c7ec91e1f7e5fb
HafidaHamcherif <58478522+HafidaHamcherif@users.noreply.github.com>
2020-07-02 22:43:05 +0200

    site jquery

---
+++ main.js
@@ -0,0 +1,18 @@
+$(function(){
+    var btnEl = $('#nextBtn');
+    btnEl.click(function(){
+        var slide = $('.carousel-slide');
+        slide.slick({
+            // autoplay:false,
+            // autoplaySpeed: 7000,
+            // dots:false,
+            // pauseOnHover:false,
+            // slidesToShow: 4,
+            // slidesToScroll: 1,
+            // infinite: true,
+    
+        });
+    });
+});
+
+
